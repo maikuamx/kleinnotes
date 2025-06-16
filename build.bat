9a55909e92b0385668c4e14b52a83c34d18d786e
johernandezvaz <johernandezvaz@gmail.com>
2025-06-16 10:24:52 -0600

    arreglar deploy#

--- build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-pip install -r requirements.txt
-python manage.py migrate
-python manage.py collectstatic --noinput
--- sigmundfiles/settings.py
+++ sigmundfiles/settings.py
@@ -72,17 +72,17 @@ WSGI_APPLICATION = 'sigmundfiles.wsgi.application'
 # }
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
